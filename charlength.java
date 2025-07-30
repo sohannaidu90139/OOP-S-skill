ef1ef977ba0c1f5cbb6e02a7e51723988c5d24b0
sohannaidu90139 <2420090139@klh.edu.in>
2025-07-30 10:37:42 +0530

    Add files via upload

---
+++ charlength.java
@@ -0,0 +1,14 @@
+package skill;
+
+public class charlength 
+{
+	    public static void main(String[] args) 
+	    {
+	        String s = "java programs";
+	        System.out.println("charAt(2): " + s.charAt(2));  
+	        System.out.println("length(): " + s.length());    
+	    }
+	
+
+
+}
